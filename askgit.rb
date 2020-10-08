945a221104f8975fddbd52d690ea545e51674631
Patrick DeVivo <patrick.devivo@gmail.com>
2020-10-08 14:16:31 -0400

    set version

--- askgit.rb
+++ askgit.rb
@@ -1,6 +1,7 @@
 class Askgit < Formula
   desc "Query git repositories with SQL. Generate reports, perform status checks, analyze codebases. 🔍 📊"
   homepage "https://askgit.com"
+  version "0.2.1"
   url "https://github.com/augmentable-dev/askgit/archive/v0.2.1.tar.gz"
   sha256 "8d93d583a7c5ca9f2fc48455290398d5d1fc93c02cfd4af2e6ff8e5a88983385"
   license "MIT"
