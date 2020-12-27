d9e96b1ac4b8a85313e476d387e9f625f9e303f3
Patrick DeVivo <patrick.devivo@gmail.com>
2020-12-27 11:08:02 -0500

    bump to v.0.3.4

--- askgit.rb
+++ askgit.rb
@@ -1,9 +1,9 @@
 class Askgit < Formula
   desc "Query git repositories with SQL. Generate reports, perform status checks, analyze codebases. 🔍 📊"
   homepage "https://askgit.com"
-  version "v0.3.3"
-  url "https://github.com/augmentable-dev/askgit/archive/v0.3.3.tar.gz"
-  sha256 "1d562e38b9f5353c29a930044b025d49ecbab7262e655ad5ab647df3a633b8bf"
+  version "v0.3.4"
+  url "https://github.com/augmentable-dev/askgit/archive/v0.3.4.tar.gz"
+  sha256 "d8b4c73fe09c24ee96c478aca76f3aeb664f9b81c6c7c1e5127074fd0fa13dc1"
   license "MIT"
 
   depends_on "go" => :build
