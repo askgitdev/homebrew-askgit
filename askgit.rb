42762de97e6c555ebebb6dc5f80487cda1cc7d4a
Patrick DeVivo <patrick.devivo@gmail.com>
2021-07-20 20:12:53 -0400

    fix version bump

--- askgit.rb
+++ askgit.rb
@@ -1,7 +1,7 @@
 class Askgit < Formula
   desc "Query git repositories with SQL. Generate reports, perform status checks, analyze codebases. 🔍 📊"
   homepage "https://askgit.com"
-  version "v0.3.7"
+  version "v0.4.0"
   url "https://github.com/augmentable-dev/askgit/archive/v0.4.0.tar.gz"
   sha256 "f3fffdd1b6cdcd77d6c55360f915061f7367d86a5b329f99ad8d59fa325f7113"
   license "MIT"
